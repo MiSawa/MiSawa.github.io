3d2aa74e6819696ae737010b1498e83f3924deea
Mi_Sawa <mi.sawa.1216+git@gmail.com>
2015-12-16 00:19:42 +0900

    Automated commit at 2015-12-15 15:19:04 UTC by middleman-deploy 1.0.0

--- javascripts/base.js
+++ javascripts/base.js
@@ -34,6 +34,18 @@ document.write("\\(\\def\\setmid{\\mathrel{}\\middle|\\mathrel{}}\\)");
 document.write("\\(\\def\\set#1{\\left\\{\\,#1\\,\\right\\}}\\)");
 document.write("</div>");
 
+$(function(){
+    $('.fold_button:not(.fold_button_processed)').each(function(){
+         $(this).addClass('fold_button_processed')
+         $(this).prepend('<tt>+</tt> ');
+         $(this).nextAll('.fold_begin').first().nextUntil('.fold_end').wrapAll("<div class='fold' />");
+         $(this).nextAll('.fold').first().toggle(false);
+         $(this).on('click', function(){
+             $(this).find('tt').text($(this).find('tt').text() == '+' ? '-' : '+');
+             $(this).nextAll('.fold').first().toggle();
+         });
+    });
+});
 
 
 
