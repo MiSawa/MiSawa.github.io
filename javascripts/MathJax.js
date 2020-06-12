240a0ec7c89571ae25d9e9a7ad16c3d7d5bdf1e4
mi_sawa <mi.sawa.1216+git@gmail.com>
2020-06-13 01:19:23 +0900

    Build with middleman v4

--- javascripts/MathJax.js
+++ javascripts/MathJax.js
@@ -1,31 +1,37 @@
-document.write("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML&delayStartupUntil=configured'></script>")
-document.write("<meta http-equiv='X-UA-Compatible' CONTENT='IE=EmulateIE7'/>")
-document.write("<script type='text/javascript'>(" + function(){
-MathJax.Hub.Config({
-    extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js",
-                 "TeX/noErrors.js","TeX/noUndefined.js"],
-    tex2jax: {
-        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-        displayMath: [ ['$$','$$'], ["\\[","\\]"] ]
-    },
-    TeX: {
-        equationNumbers: { autoNumber: "AMS" },
-        Macros: {
-            N: '{\\mathbb{N}}',
-            Z: '{\\mathbb{Z}}',
-            Q: '{\\mathbb{Q}}',
-            R: '{\\mathbb{R}}',
-            C: '{\\mathbb{C}}',
-            divides: '{\\mid}',
-            abs: ['{\\left| #1 \\right|}', 1],
+document.write("<script async type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS-MML_HTMLorMML&delayStartupUntil=configured'></script>");
+document.write("<meta http-equiv='X-UA-Compatible' CONTENT='IE=EmulateIE7'/>");
+(function(){
+    const configureOrRetry = () => {
+        if (typeof MathJax === 'undefined') {
+            setTimeout(configureOrRetry, 10);
+            return;
         }
-    },
-});
-MathJax.Hub.Configured();
-}.toString() + ")()</script>");
-document.write("<div style=\"display:none\">");
-document.write("\\(\\def\\relmiddle#1{\\mathrel{}\\middle#1\\mathrel{}}\\)");
-document.write("\\(\\def\\setmid{\\mathrel{}\\middle|\\mathrel{}}\\)");
-document.write("\\(\\def\\set#1{\\left\\{\\,#1\\,\\right\\}}\\)");
-document.write("</div>");
+        MathJax.Hub.Config({
+            extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js",
+                "TeX/noErrors.js","TeX/noUndefined.js"],
+            tex2jax: {
+                inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+                processExcapes: true
+            },
+            TeX: {
+                equationNumbers: { autoNumber: "AMS" },
+                Macros: {
+                    N: '{\\mathbb{N}}',
+                    Z: '{\\mathbb{Z}}',
+                    Q: '{\\mathbb{Q}}',
+                    R: '{\\mathbb{R}}',
+                    C: '{\\mathbb{C}}',
+                    divides: '{\\mid}',
+                    abs: ['{\\left| #1 \\right|}', 1],
+                    relmiddle: ['\\mathrel{}\\middle#1\\mathrel{}', 1],
+                    setmid: '\\relmiddle|',
+                    set: ['\\left\\{\\,#1\\,\\right\\}', 1],
+                }
+            },
+        });
+        MathJax.Hub.Configured();
+    };
+    configureOrRetry();
+})();
 
