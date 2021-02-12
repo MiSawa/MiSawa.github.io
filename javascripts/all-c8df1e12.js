75c3c822646f38195adbb6f754e257ee4a13f913
mi_sawa <mi.sawa.1216+git@gmail.com>
2021-02-12 23:01:24 +0900

    Remove foldh3

--- javascripts/all-c8df1e12.js
+++ javascripts/all-c8df1e12.js
@@ -53,23 +53,3 @@ $(function(){
 
 
 
-$(function(){
-    $('#main>h3').each(function(){
-        $(this).addClass('fold_button');
-        $(this).after("<pre class='fold_begin'></pre>");
-        $(this).nextAll('h1,h2,h3').first().before("<pre class='fold_end'></pre>");
-    });
-});
-$(function(){
-    $('.fold_button:not(.fold_button_processed)').each(function(){
-         $(this).addClass('fold_button_processed')
-         $(this).prepend('<span class="fold-plus">+</span> ');
-         $(this).nextAll('.fold_begin').first().nextUntil('.fold_end').wrapAll("<div class='fold' />");
-         $(this).nextAll('.fold').first().toggle(false);
-         $(this).on('click', function(){
-             const elem=$(this).find('.fold-plus');
-             elem.text(elem.text() == '+' ? '-' : '+');
-             $(this).nextAll('.fold').first().toggle();
-         });
-    });
-});
